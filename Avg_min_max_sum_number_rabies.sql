fad3a6bcac9e07c21b4f79c24b78d909be03732c
Hanna Dunska <46302903+Ember1995@users.noreply.github.com>
2025-03-04 19:35:02 +0100

    Add files via upload

---
+++ Avg_min_max_sum_number_rabies.sql
@@ -0,0 +1,13 @@
+USE pandemic;
+
+SELECT
+	ic.entity_id AS entity_id,
+    AVG(Number_rabies) AS avg_number_rabies,
+    MIN(Number_rabies) AS min_number_rabies,
+    MAX(Number_rabies) AS max_number_rabies, 
+    SUM(Number_rabies) AS sum_number_rabies
+FROM infectious_cases ic
+WHERE Number_rabies <> ''
+GROUP BY ic.entity_id
+ORDER BY avg_number_rabies DESC
+LIMIT 10;
